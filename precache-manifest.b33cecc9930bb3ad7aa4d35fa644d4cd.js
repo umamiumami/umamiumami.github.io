85ecf8a9d5bcbab5e27883020e63c46b3450c789
Jimmy Hong <jimmyhong.rocks@gmail.com>
2020-09-19 13:22:58 -0500

    Updates

---
+++ precache-manifest.b33cecc9930bb3ad7aa4d35fa644d4cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c487afe104ae13090a03d840b6cb55b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db4ac18dd4f1e4068a9e",
+    "url": "/static/js/2.bc217734.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bc217734.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36ce4d36197bb9544fa1",
+    "url": "/static/js/main.02c101ca.chunk.js"
+  },
+  {
+    "revision": "a3e23fead4f02db2a8c0",
+    "url": "/static/js/runtime-main.fcf36234.js"
+  }
+]);
