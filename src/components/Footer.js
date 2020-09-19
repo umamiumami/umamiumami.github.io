73f213acdfe3fe8764fb475b872c0b5d543cacc6
Jimmy Hong <jimmyhong.rocks@gmail.com>
2020-09-19 12:42:36 -0500

    initial template setup

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer id="footer">
+      <p className="copyright">
+        &copy; Umami Sushi <a href="https://html5up.net">512-667-7903</a>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
