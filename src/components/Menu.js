943b46cd61b8a64738c2b12067f5f94cbee29b23
Jimmy Hong <jimmyhong.rocks@gmail.com>
2020-09-19 14:39:39 -0500

    create menu and view pdf

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,31 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import { Document, Page } from "react-pdf";
+import MenuFile from "../assets/menu.pdf";
 
 function Menu() {
+  const [numPages, setNumPages] = useState(null);
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
   return (
     <article id="intro">
-      <h2 className="major">Intro</h2>
-      <span className="image main">
-        <img src="images/pic01.jpg" alt="" />
-      </span>
-      <p>
-        Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin aliquam
-        facilisis ante interdum congue. Integer mollis, nisl amet convallis,
-        porttitor magna ullamcorper, amet egestas mauris. Ut magna finibus nisi
-        nec lacinia. Nam maximus erat id euismod egestas. By the way, check out
-        my <a href="#work">awesome work</a>.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus
-        rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh
-        porttitor fermentum. Nullam venenatis erat id vehicula viverra. Nunc
-        ultrices eros ut ultricies condimentum. Mauris risus lacus, blandit sit
-        amet venenatis non, bibendum vitae dolor. Nunc lorem mauris, fringilla
-        in aliquam at, euismod in lectus. Pellentesque habitant morbi tristique
-        senectus et netus et malesuada fames ac turpis egestas. In non lorem sit
-        amet elit placerat maximus. Pellentesque aliquam maximus risus, vel sed
-        vehicula.
-      </p>
+      <h2 className="major">Menu</h2>
+      <h5>
+        <a href={MenuFile} target="_blank" rel="noopener noreferrer">
+          View in Full Screen
+        </a>
+      </h5>
+      <Document
+        file={MenuFile}
+        options={{ workerSrc: "/pdf.worker.js" }}
+        onLoadSuccess={onDocumentLoadSuccess}
+      >
+        {Array.from(new Array(numPages), (el, index) => (
+          <Page
+            key={`page_${index + 1}`}
+            pageNumber={index + 1}
+            loading="loading menu..."
+          />
+        ))}
+      </Document>
     </article>
   );
 }
